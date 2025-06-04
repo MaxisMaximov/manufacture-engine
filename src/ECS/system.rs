c11258e563f956acb989b1a4f9e7ed7b2207b023
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-06-04 13:33:46 +0200

    Wires up Query to System and adjusted the System traits a bit

--- src/ECS/system.rs
+++ src/ECS/system.rs
@@ -1,35 +1,39 @@
 use super::*;
 
 use world::*;
+use fetch::*;
 
 pub trait gmSystem{
     #[cfg(not(query_data))]
-    type sysData<'a>: gmSystemData<'a>;
+    type sysData: gmSystemData;
     #[cfg(query_data)]
-    type QUERY: fetch::QueryData;
-
-    const sysDepends: &'static [&'static str];
+    type QUERY: QueryData;
 
     fn new() -> Self;
     fn SYS_ID() -> &'static str;
+    fn SYS_DEPENDS() -> &'static [&'static str];
+
+    
 
     #[cfg(not(query_data))]
-    fn execute<'a>(&mut self, IN_data: Self::sysData<'a>);
+    fn execute(&mut self, IN_data: Self::sysData);
     #[cfg(query_data)]
-    fn execute<'a>(&mut self, IN_data: Self::QUERY);
+    fn execute<'a>(&mut self, IN_data: Query<'_, Self::QUERY>);
 }
 
-pub trait gmSysRun{
-    fn executeNow<'a>(&mut self, IN_world: &'a mut gmWorld);
+pub trait gmSystemWrapper{
+    fn execute<'a>(&mut self, IN_world: &'a mut gmWorld);
 }
-impl<T> gmSysRun for T where T:gmSystem{
-    fn executeNow<'a>(&mut self, IN_world: &'a mut gmWorld) {
+impl<T: gmSystem> gmSystemWrapper for T{
+    
+    fn execute<'a>(&mut self, IN_world: &'a mut gmWorld) {
         #[cfg(not(query_data))]
         self.execute(T::sysData::fetch(IN_world));
 
         #[cfg(query_data)]
-        self.execute(T::QUERY::fetch(IN_world))
+        self.execute(Query::fetch(IN_world))
     }
+}
 }
 
 pub trait gmSystemData<'a>{
