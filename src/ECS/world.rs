3c5c4b88b69b0be4fe88d541682a4cbc25d584b6
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-06-09 13:58:40 +0200

    Cleaned up `gmWorld` functions

--- src/ECS/world.rs
+++ src/ECS/world.rs
@@ -32,37 +32,55 @@ impl gmWorld{
         }
     }
 
-pub fn fetch<'a, T>(&'a self) -> Fetch<'a, T> where T: Component + 'static{
-        Ref::map(self.components.get(T::ID)
-        .expect(&format!("ERROR: Tried to fetch an unregistered component: {}", T::ID))
-        .borrow(), 
-        |idkfa| &idkfa.downcast::<T>().inner)
+    pub fn fetch<'a, T>(&'a self) -> Fetch<'a, T> where T: Component + 'static{
+        // Check if we have such component in the first place
+        if !self.components.contains_key(T::ID){
+            // There's no way to signify missing components yet, so we panic for now
+            panic!("ERROR: Tried to fetch an unregistered component: {}", T::ID)
+        }
+
+        Ref::map(
+            // Unwrap: We have a check for an invalid Component earlier
+            self.components.get(T::ID).unwrap().borrow(), 
+            |idkfa| &idkfa.downcast::<T>().inner)
     }
-pub fn fetchMut<'a, T>(&'a self) -> FetchMut<'a, T> where T: Component + 'static{
-        RefMut::map(self.components.get(T::ID)
-        .expect(&format!("ERROR: Tried to fetch an unregistered component: {}", T::ID))
-        .borrow_mut(), 
-        |idkfa| &mut idkfa.downcast_mut::<T>().inner)
+    pub fn fetchMut<'a, T>(&'a self) -> FetchMut<'a, T> where T: Component + 'static{
+        // Same as above
+        if !self.components.contains_key(T::ID){
+            panic!("ERROR: Tried to fetch an unregistered component: {}", T::ID)
+        }
+
+        RefMut::map(
+            self.components.get(T::ID).unwrap().borrow_mut(), 
+            |idkfa| &mut idkfa.downcast_mut::<T>().inner)
     }
 
     pub fn fetchRes<'a, T>(&'a self) -> FetchRes<'a, T> where T: gmRes + 'static{
+        // Check if we have such Resource registered already
+        if !self.resources.contains_key(T::RES_ID()){
+            // Same as with Component fetch
+            panic!("ERROR: Tried to fetch an unregistered resource: {}", T::RES_ID())
+        }
+
         Ref::map(
-            self.resources.get(T::RES_ID())
-            .expect(&format!("ERROR: Tried to fetch an unregistered resource: {}", T::RES_ID()))
-            .borrow(), 
+            self.resources.get(T::RES_ID()).unwrap().borrow(), 
             |idkfa| idkfa.downcast_ref::<T>().unwrap())
     }
     pub fn fetchResMut<'a, T>(&'a self) -> FetchResMut<'a, T> where T: gmRes + 'static{
-            RefMut::map(self.resources.get(T::RES_ID())
-            .expect(&format!("ERROR: Tried to fetch an unregistered resource: {}", T::RES_ID()))
-            .borrow_mut(), 
+        // Same as above
+        if !self.resources.contains_key(T::RES_ID()){
+            panic!("ERROR: Tried to fetch an unregistered resource: {}", T::RES_ID())
+        }
+
+        RefMut::map(
+            self.resources.get(T::RES_ID()).unwrap().borrow_mut(), 
             |idkfa| idkfa.downcast_mut::<T>().unwrap())
     }
 
     pub fn fetchEventReader<'a, T>(&'a self) -> EventReader<'a, T> where T: gmEvent + 'static{
         // SAFETY: Right now the entirety of the engine is single-threaded
         // So we don't have to worry about two systems on sepparate threads
-        // colliding when they send a new event
+        // colliding when they send/receive a new event
         //
         // This is because `EventMap` initializes a new queue for an event 
         // if the queue for the current frame doesn't yet exist
@@ -77,25 +95,24 @@ pub fn fetchMut<'a, T>(&'a self) -> FetchMut<'a, T> where T: Component + 'static
     }
 
     pub fn registerComp<T>(&mut self) where T: Component + 'static{
-        use std::collections::hash_map::Entry;
-        match self.components.entry(T::ID){
-            Entry::Occupied(_) => panic!("ERROR: Attempted to override an existing component: {}", T::ID),
-            Entry::Vacant(ENTRY) => ENTRY.insert(
-                RefCell::new(
-                    Box::new(
-                    gmStorageContainer::<T>{inner: T::STORAGE::new()})))
-        };
+        if !self.components.contains_key(T::ID){
+            panic!("ERROR: Attempted to override an existing component: {}", T::ID)
+        }
+
+        self.components.insert(
+            T::ID, 
+            RefCell::new(Box::new(gmStorageContainer::<T>{inner: T::STORAGE::new()})));
     }
     pub fn unRegisterComp<T>(&mut self) where T: Component + 'static{
         self.components.remove(T::ID);
     }
 
     pub fn registerRes<T>(&mut self) where T: gmRes + 'static{
-        use std::collections::hash_map::Entry;
-        match self.resources.entry(T::RES_ID()){
-            Entry::Occupied(_) => panic!("ERROR: Attempted to override an existing resource: {}", T::RES_ID()),
-            Entry::Vacant(ENTRY) => ENTRY.insert(RefCell::new(Box::new(T::new())))
-        };
+        if !self.resources.contains_key(T::RES_ID()){
+            panic!("ERROR: Attempted to override an existing resource: {}", T::RES_ID())
+        }
+
+        self.resources.insert(T::RES_ID(), RefCell::new(Box::new(T::new())));
     }
     pub fn unRegisterRes<T>(&mut self) where T: gmRes + 'static{
         self.resources.remove(T::RES_ID());
@@ -137,6 +154,7 @@ pub fn fetchMut<'a, T>(&'a self) -> FetchMut<'a, T> where T: Component + 'static
     }
 
     pub fn commandsExec(&mut self){
+
         loop{
             let idkfa = self.commands.borrow_mut().pop();
             match idkfa{
