71e69e5f9eb4edb01fc3df00e68f13c5517f774d
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-06-16 13:34:34 +0200

    Added documentation to the Command trait

--- src/ECS/commands.rs
+++ src/ECS/commands.rs
@@ -1,45 +1,37 @@
 use std::any::Any;
 use super::world::gmWorld;
 
+/// # Command trait
+/// Defines a command that does an operation on the whole World
+/// 
+/// Typically used to add/remove components from entities, 
+/// spawn/despawn entities, register new resources/components etc.
+/// 
+/// ID is completely optional for debug purposes
 pub trait Command: Any{
-    const ID: &'static str;
+    const ID: &'static str = "idkfa";
+    /// Execute the Command on specified World
     fn execute(&mut self, World: &mut gmWorld);
 }
 
+/// # Command trait Wrapper
+/// A wrapper trait for Commands to safely store them within the World
+/// 
+/// Provides ID method for identifying the command (if such ID was specified) 
+/// and execute method for executing the command on the specified World
 pub trait CommandWrapper{
     fn id(&self) -> &'static str;
     fn execute(&mut self, World: &mut gmWorld);
 }
 
 impl<T: Command> CommandWrapper for T{
+    /// Get the underlying Command's ID
     fn id(&self) -> &'static str {
         T::ID
     }
 
+    /// Execute the Command on the specified World
     fn execute(&mut self, World: &mut gmWorld) {
         Command::execute(self, World);
-    }
-}
-
-impl dyn CommandWrapper{
-    pub fn downcast_ref<T: Command>(&self) -> Option<&T>{
-        if T::ID == self.id(){
-            // SAFETY: We have a check for matching IDs beforehand
-            Some(unsafe{
-                &*(self as *const dyn CommandWrapper as *const T)
-            })
-        }else{
-            None
-        }
-    }
-    pub fn downcast_mut<T: Command>(&mut self) -> Option<&mut T>{
-        if T::ID == self.id(){
-            // SAFETY: We have a check for matching IDs beforehand
-            Some(unsafe {
-                &mut *(self as *mut dyn CommandWrapper as *mut T)
-            })
-        }else{
-            None
-        }
     }
 }
