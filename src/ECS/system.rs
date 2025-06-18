e6d6f896e1cffbfdfda71031f3c000b4b451d1ca
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-06-18 11:57:21 +0200

    Added documentation to the System trait

--- src/ECS/system.rs
+++ src/ECS/system.rs
@@ -1,20 +1,41 @@
 use super::*;
 
-use world::*;
-use fetch::*;
+use world::gmWorld;
+use fetch::{QueryData, Query};
 
+/// # System trait
+/// Defines a System that will be run on the World
+/// 
+/// `QUERY` is the components the system wants from the Word
+/// 
+/// `ID` is what the system will be identified by for future overrides
+/// 
+/// `DEPENDS` are the other Systems that must be run before the System can be
+/// 
+/// ## WARNING
+/// Make sure your System ID does not collide with Systems fro other plugins
 pub trait System{
-    type QUERY: fetch::QueryData;
+    type QUERY: QueryData;
     const ID: &'static str;
     const DEPENDS: &'static [&'static str];
 
+    /// Create a new instance of this system
     fn new() -> Self;
+    /// Run the system
     fn execute(&mut self, Data: Query<'_, Self::QUERY>);
 }
 
+/// # System trait Wrapper
+/// A wrapper trait for Systems to safely dispatch them in the Dispatcher
+/// 
+/// Provides ID method for identifying the underlying System, Depends method for getting the it's dependencies
+/// and Execute method for running it
 pub trait SystemWrapper{
+    /// Get the underlying System's ID
     fn id(&self) -> &'static str;
+    /// Get the underlying system's dependencies
     fn depends(&self) -> &'static [&'static str];
+    /// Run the underlying system with specified World
     fn execute<'a>(&mut self, World: &'a mut gmWorld);
 }
 
