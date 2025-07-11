66bb6bf3786a5140410d47c9b4746434bf1722ba
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-07-11 15:15:48 +0200

    Added a constructor method to the Dispatcher

--- src/ECS/dispatcher/mod.rs
+++ src/ECS/dispatcher/mod.rs
@@ -80,6 +80,44 @@ impl Dispatcher{
             stages: Vec::new(),
         }
     }
+    pub fn with<S: System>(mut self) -> Self{
+        if self.registry.contains_key(S::ID){
+            panic!("ERROR: System {} already exists\nOverrides are not yet supported, if that's what you wanted to do", S::ID);
+        }
+
+        // Check what stage the system would ideally be in
+        let mut ideal_stage = 0;
+        for dep in S::DEPENDS{
+            if !self.registry.contains_key(dep){
+                panic!("ERROR: System {}'s dependency {} does not exist", S::ID, dep)
+            }
+            // The latest stage a dependency is in is the ideal stage
+            ideal_stage = ideal_stage.max(*self.registry.get(dep).unwrap());
+        }
+        
+        // Keep looping until we find a compatible stage
+        loop{
+            match self.stages.get_mut(ideal_stage){
+                Some(stage) => {
+                    // Check if the stage is at it's limits
+                    if !stage.len() == 5{
+                        stage.push(Box::new(S::new()));
+                        break;
+                    }else{
+                        ideal_stage += 1
+                    }
+                },
+                None => {
+                    let mut stage: Vec<Box<dyn SystemWrapper>> = Vec::new();
+                    stage.push(Box::new(S::new()));
+                    self.stages.push(stage);
+                    break;
+                }
+            }
+        }
+
+        self
+    }
     pub fn dispatch(&mut self, World: &mut World){
         for stage in self.stages.iter_mut(){
             for system in stage.iter_mut(){
