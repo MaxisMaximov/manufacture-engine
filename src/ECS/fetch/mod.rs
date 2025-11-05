ba92be14f737275a707efc2419659a00bca35c2d
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-05 13:45:24 +0100

    Impld methods for Event Reader/Writer

--- src/ECS/fetch/mod.rs
+++ src/ECS/fetch/mod.rs
@@ -18,9 +18,29 @@ pub type FetchResMut<'a, R: Resource> = RefMut<'a, R>;
 pub struct EventReader<'a, E: Event>{
     pub(super) inner: Ref<'a, Vec<E>>
 }
+impl<E: Event> EventReader<'_, E>{
+    pub fn read_iter(&self) -> impl Iterator<Item = &E>{
+        self.inner.iter()
+    pub fn event_count(&self) -> usize{
+        self.inner.len()
+    }
+    }
 pub struct EventWriter<'a, E: Event>{
     pub(super) inner: RefMut<'a, Vec<E>>
 }
+impl<E: Event> EventWriter<'_, E>{
+    pub fn iter(&self) -> impl Iterator<Item = &E>{
+        self.inner.iter()
+    }
+    pub fn event_count(&self) -> usize{
+        self.inner.len()
+    }
+    pub fn send(&mut self, Event: E){
+        self.inner.push(Event);
+    }
+}
+}
+    }
 
 ///////////////////////////////////////////////////////////////////////////////
 // Reexports
