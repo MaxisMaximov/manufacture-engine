328a665ab7c83f2e251d6cb9bd7ca409c4679aee
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-06 10:30:55 +0100

    Added update methods to DeltaT Unfortunately I can't restrict it to just the Dispatcher, sadge

--- src/core/resources.rs
+++ src/core/resources.rs
@@ -5,7 +5,7 @@ use ECS::resource::Resource;
 /// # Delta Time Resources
 /// Tracks milliseconds elapsed since last frame and last Logic run. Use provided `delta_frame` and `delta_logic` methods to get the time
 /// 
-/// Only the Dispatcher can modify the inner values, fetching this mutably does not do anything
+/// Only the Dispatcher is allowed modify the inner values, **DO NOT SET IT MANUALLY**
 pub struct DeltaT{
     delta_frame: u128,
     delta_logic: u128
@@ -18,6 +18,14 @@ impl DeltaT{
     /// Get milliseconds elapsed since last time Logic ran
     pub fn delta_logic(&self) -> u128{
         self.delta_logic
+    }
+
+    pub fn set_delta_frame(&mut self, Delta: u128){
+        self.delta_frame = Delta
+    }
+
+    pub fn set_delta_logic(&mut self, Delta: u128){
+        self.delta_logic = Delta
     }
 }
 impl Resource for DeltaT{
