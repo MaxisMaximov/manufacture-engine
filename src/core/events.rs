3e1d619111c483da54b55427d8a82a4729cc911e
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-11-28 13:57:41 +0100

    Added Entity Spawn/Despawn events

--- src/core/events.rs
+++ src/core/events.rs
@@ -1,1 +1,15 @@
 use super::*;
+
+/// Announces what Entity has just spawned
+/// 
+/// Provides a Token alongside for Systems to copy
+pub struct EntitySpawned(pub Token);
+impl Event for EntitySpawned{
+    const ID: &'static str = "EntitySpawned";
+}
+
+/// Announced what Entity has just despawned
+pub struct EntityDespawned(pub usize);
+impl Event for EntityDespawned{
+    const ID: &'static str = "EntityDespawned";
+}
